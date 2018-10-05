bee4014ef41e7ac5a01cff101eb87520ae0a76d5
Ryan Emerson <remerson@redhat.com>
2018-10-05 15:34:40 +0100

    JDG-2214 Enable custom server configuration in openshift via configmaps

--- modules/datagrid/72/launch/added/launch/openshift-common.sh
+++ modules/datagrid/72/launch/added/launch/openshift-common.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 if [ "${SCRIPT_DEBUG}" = "true" ] ; then
-    set -x
-    echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
+  set -x
+  echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
 fi
 
-CONFIG_FILE=$JBOSS_HOME/standalone/configuration/clustered-openshift.xml
 LOGGING_FILE=$JBOSS_HOME/standalone/configuration/logging.properties
+if [ "${USER_CONFIG_MAP^^}" != "TRUE" ]; then
+  CONFIG_FILE=$JBOSS_HOME/standalone/configuration/clustered-openshift.xml
 
-CONFIGURE_SCRIPTS=(
+  CONFIGURE_SCRIPTS=(
   $JBOSS_HOME/bin/launch/backward-compatibility.sh
   $JBOSS_HOME/bin/launch/configure_extensions.sh
   $JBOSS_HOME/bin/launch/passwd.sh
@@ -26,4 +27,25 @@ CONFIGURE_SCRIPTS=(
   $JBOSS_HOME/bin/launch/management-realm.sh
   $JBOSS_HOME/bin/launch/access_log_valve.sh
   /opt/run-java/proxy-options
-)
+  )
+else
+  CONFIG_FILE=$JBOSS_HOME/standalone/configuration/standalone.xml
+
+  CONFIGURE_SCRIPTS=(
+  $JBOSS_HOME/bin/launch/backward-compatibility.sh
+  $JBOSS_HOME/bin/launch/configure_extensions.sh
+  $JBOSS_HOME/bin/launch/passwd.sh
+  $JBOSS_HOME/bin/launch/authentication-config.sh
+  $JBOSS_HOME/bin/launch/datasource.sh
+  $JBOSS_HOME/bin/launch/admin.sh
+  $JBOSS_HOME/bin/launch/ha.sh
+  $JBOSS_HOME/bin/launch/jgroups.sh
+  $JBOSS_HOME/bin/launch/https.sh
+  $JBOSS_HOME/bin/launch/json_logging.sh
+  $JBOSS_HOME/bin/launch/security-domains.sh
+  $JBOSS_HOME/bin/launch/jboss_modules_system_pkgs.sh
+  $JBOSS_HOME/bin/launch/management-realm.sh
+  $JBOSS_HOME/bin/launch/access_log_valve.sh
+  /opt/run-java/proxy-options
+  )
+fi
