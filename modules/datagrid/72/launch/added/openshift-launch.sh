97cb3b143b0e690a068acea748d7b49725f565b9
Ryan Emerson <remerson@redhat.com>
2018-10-05 15:34:40 +0100

    JDG-2214 Enable custom server configuration in openshift via configmaps

--- modules/datagrid/72/launch/added/openshift-launch.sh
+++ modules/datagrid/72/launch/added/openshift-launch.sh
@@ -23,10 +23,25 @@ function init_data_dir() {
   fi
 }
 
+function copy_user_files() {
+  local config_dir=${JBOSS_HOME}/standalone/configuration
+  # Remove default standalone.xml so that an error is thrown if the user does not pass a standalone.xml file in the configmap
+  rm $config_dir/standalone.xml
+  cp $config_dir/user/* $config_dir
+}
+
 SPLIT_DATA=${SPLIT_DATA:-$DATAGRID_SPLIT}
 SPLIT_LOCK_TIMEOUT=${SPLIT_LOCK_TIMEOUT:-$DATAGRID_LOCK_TIMEOUT}
 
-if [ "${SPLIT_DATA^^}" = "TRUE" ]; then
+# If USER_CONFIG_MAP is true, then we launch the server using the standalone.xml provided by the user in a configmap
+if [ "${USER_CONFIG_MAP^^}" = "TRUE" ]; then
+  copy_user_files
+
+  log_info "Running $JBOSS_IMAGE_NAME image, version $JBOSS_IMAGE_VERSION with user standalone.xml"
+  source $JBOSS_HOME/bin/launch/configure.sh
+  exec $JBOSS_HOME/bin/standalone.sh -bmanagement 127.0.0.1 ${JBOSS_HA_ARGS} ${JAVA_PROXY_OPTIONS}
+
+elif [ "${SPLIT_DATA^^}" = "TRUE" ]; then
   source /opt/partition/partitionPV.sh
 
   DATA_DIR="${JBOSS_HOME}/standalone/partitioned_data"
