fe35e8e874b2bf5b64ec0dc458ffcb9d28b88627
Osni Oliveira <osni.oliveira@redhat.com>
2019-11-12 01:17:51 -0300

    Change the image to be based on UBI8
    This is in preparation to have a multi-arch build for both Intel (x86_64) and
    IBM Z (s390x). However not mandatory (we could base on RHEL7), it was put as
    a desirable requirement - and might easy the process in some points.
    
    Changes/fixes include:
    - Change the base image
    - Change the package manager to dnf
    - Remove install of yum-utils
    - Remove modules not available in UBI8 (including database drivers for
      postgresql and mysql - also remove corresponding datasource definitions)
    - Change Maven installation method (from using Software Collections to using
      common packages)
    
    References: IBMZP-28, JDG-3368
    
    WARNING: the image might be in a non-buildable state after this commit,
      required changes might be present in future commits.

--- modules/common/jboss/container/maven/default/bash/backward_compatibility.sh
+++ modules/common/jboss/container/maven/default/bash/backward_compatibility.sh
@@ -4,7 +4,5 @@ set -e
 
 # For backward compatibility
 mkdir -p /usr/local/s2i
-ln -s /opt/jboss/container/maven/default/scl-enable-maven /usr/local/s2i/scl-enable-maven
-chown -h jboss:root /usr/local/s2i/scl-enable-maven
 ln -s /opt/jboss/container/maven/default/maven.sh /usr/local/s2i/common.sh
 chown -h jboss:root /usr/local/s2i/common.sh
